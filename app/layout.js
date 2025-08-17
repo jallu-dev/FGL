d268c77307eb1875b80a3f296a082715b5c1296c
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2025-08-17 20:17:07 +0530

    suspense boundary added

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ReactQueryProvider from "@/utils/providers/ReactQueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const playfair = Playfair_Display({
@@ -19,11 +22,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${playfair.variable}`}>
-        <ReactQueryProvider>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-        </ReactQueryProvider>
+        <Suspense>
+          <ReactQueryProvider>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <Toaster />
+            <ReactQueryDevtools />
+          </ReactQueryProvider>
+        </Suspense>
       </body>
     </html>
   );
